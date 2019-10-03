1f7a199585c4fe6d6b0a44bd7dd552bffc1a14db
parmeshwar92 <31009067+parmeshwar92@users.noreply.github.com>
2019-10-03 14:28:47 +0530

    splitting code
    splitting code

--- src/index.js
+++ src/index.js
@@ -1,26 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles.css";
-import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
-import JsonTable from "./jsonbasedtable";
-import AddItem from "./additems";
-import NotFound from "./notfound";
+import App from "./app";
 
-
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <Router>
-          <Switch>
-            <Route exact path="/" component={JsonTable} />
-            <Route exact path="/additems" component={AddItem} />
-            <Route component={NotFound} />
-          </Switch>
-        </Router>
-      </div>
-    );
-  }
-}
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
